e3892a7e7e76c7382b76899a7c722829a60341da
Koerich <130242824+Koerichj@users.noreply.github.com>
2023-10-09 13:53:50 -0300

    Add files via upload

---
+++ Algoritmo2/Algoritmo_30.pas
@@ -0,0 +1,29 @@
+Program Algoritmo_30;
+	Var
+		iNum 			:integer;
+		iDivisor	:integer;
+
+Function Divisor(iNum:integer):integer;
+	Var
+		i				:integer;			
+		iDiv		:integer;
+Begin
+	iDiv := 0;
+	for i:= 1 to iNum do
+		if (iNum mod i) = 0 then
+		begin
+			iDiv := iDiv + 1;
+			write('[',i,']');
+		end;
+		writeln('');
+	Divisor := iDiv;
+End;
+                        
+Begin
+	write('Informe um valor: ');
+	readln(iNum);
+	writeln('');
+	iDivisor := Divisor(iNum);
+	writeln('');
+	writeln('QTD de divisores: ',iDivisor);
+End.
---
+++ Algoritmo2/Ex2 - Registro.pas
@@ -0,0 +1,72 @@
+Program Ex2_registro ;
+
+const
+	qtdAlunos  = 10;
+	
+	
+type Alunos = record
+	sNome    : string;
+	sCPF  : string;
+	sCurso : string;
+	rMedia : real;
+	bMediaSup : Boolean;
+end;
+
+type Estudante = array[1..qtdAlunos]of Alunos;
+
+var
+	i      : integer;
+	Media  : real;
+	Aluno  : Alunos;
+	aAluno : Estudante;
+	
+Begin
+		
+	//Entrada de Dados
+  for i:=1 to qtdAlunos do
+  begin
+  	write('informe o nome do aluno: ');
+  	readln(Aluno.sNome);
+  	write('informe o CPF do aluno: ');
+  	readln(Aluno.sCPF);
+  	write('informe o curso do aluno: ');
+  	readln(Aluno.sCurso);
+  	write('informe a média do aluno: ');
+  	readln(Aluno.rMedia);
+  	aAluno[i]:= Aluno;
+  	Media:= Aluno.rMedia + Media;
+		ClrScr;	
+	end;
+  
+  //Calculo para Media dos Alunos
+  Media:= Media / qtdAlunos;
+  
+  //Estrutura para saber se o aluno tem
+	//media superior que a turma
+  for i:= 1 to qtdAlunos do
+  begin
+  	if aAluno[i].rMedia > Media then
+  		aAluno[i].bMediaSup:=	True;
+  end;
+  
+  //Saida de Dados
+  writeln('------------------');
+	writeln('MÉDIA GERAL: ',Media:6:2);
+  writeln('------------------'); 
+  
+	for i:= 1 to qtdAlunos do
+  begin
+    writeln('Nome: ',aAluno[i].sNome);
+    writeln('CPF: ',aAluno[i].sCPF);
+    writeln('Curso: ',aAluno[i].sCurso);
+    writeln('Média: ',aAluno[i].rMedia:6:2);
+    if aAluno[i].bMediaSup then
+    	writeln('Média Superior: Sim')
+    else
+    	writeln('Média Superior: Não');
+    writeln('------------------');
+  end;
+  
+  
+  
+End.
